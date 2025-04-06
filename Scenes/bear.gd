35b6c5afae87a0247f6efdf8d138ef2840e9e4ec
Eriko <erikonitu@gmail.com>
2025-04-06 13:31:36 +0300

    Last change

--- Scenes/bear.gd
+++ Scenes/bear.gd
@@ -3,6 +3,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("yo")
-		var renpy_game_exe = "C:/Users/Paul/Desktop/GODOT/WINNER/Ren'Py/END_GAME-1.0-pc/END_GAME-1.0-pc/END_GAME.exe"
+		var renpy_game_exe = "C:/Users/User1/Desktop/joc godot/joc/Ren'Py/END_GAME-1.0-pc/END_GAME-1.0-pc/END_GAME.exe"
 		OS.shell_open(renpy_game_exe)
 		get_tree().reload_current_scene()
